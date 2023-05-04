423e54f3ba0b2a3ca6312e506c7daf172ed403a5
Mikhail Kaplan <kaplan.mikhail@airslate.com>
2023-05-04 23:50:16 +0300

    Cycles Task 2

---
+++ src/main/java/cycles/Factorial.java
@@ -0,0 +1,18 @@
+package cycles;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите целое число:");
+        int num = scanner.nextInt();
+        int fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println(fact);
+    }
+}
